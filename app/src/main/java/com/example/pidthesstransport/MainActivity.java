ce1b2c76668e9d40e332d9a814e093da968aa2ef
Dimitris Tetepoulidis <48908714+jimtete@users.noreply.github.com>
2021-03-21 16:39:03 +0200

    added fb login button

--- app/src/main/java/com/example/pidthesstransport/MainActivity.java
+++ app/src/main/java/com/example/pidthesstransport/MainActivity.java
@@ -5,14 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    private LoginButton login;
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login_button);
+
         infoTextView=findViewById(R.id.info);
         infoTextView.setText("Geia soy Nigger");
+
+        callbackManager = CallbackManager.Factory.create();
+
+        login.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                System.out.println("User id : " + loginResult.getAccessToken().getUserId());
+
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
 
     }
     TextView infoTextView;
