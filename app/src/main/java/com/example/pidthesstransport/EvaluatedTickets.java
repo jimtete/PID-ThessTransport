31810db889605d5364704ab266d7cf2abe824133
Dimitris Tetepoulidis <48908714+jimtete@users.noreply.github.com>
2021-04-25 10:40:54 +0300

    Adding Evaluating Tickets classes

---
+++ app/src/main/java/com/example/pidthesstransport/EvaluatedTickets.java
@@ -0,0 +1,42 @@
+package com.example.pidthesstransport;
+import java.util.Date;
+
+
+public class EvaluatedTickets {
+
+    private Date validationTime;
+    private Tickets ticket;
+    private Bus bus;
+
+    public EvaluatedTickets(Tickets ticket, Bus bus) {
+        this.validationTime = new Date();
+        this.ticket = ticket;
+        this.bus = bus;
+    }
+
+    public EvaluatedTickets(){}
+
+    public Date getValidationTime() {
+        return validationTime;
+    }
+
+    public void setValidationTime(Date validationTime) {
+        this.validationTime = validationTime;
+    }
+
+    public Tickets getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(Tickets ticket) {
+        this.ticket = ticket;
+    }
+
+    public Bus getBus() {
+        return bus;
+    }
+
+    public void setBus(Bus bus) {
+        this.bus = bus;
+    }
+}
---
+++ app/src/main/java/com/example/pidthesstransport/TicketHistory.java
@@ -0,0 +1,29 @@
+package com.example.pidthesstransport;
+
+import java.util.ArrayList;
+
+public class TicketHistory {
+
+    private ArrayList<EvaluatedTickets> evaluatedTicketsArray;
+
+    public TicketHistory(ArrayList<EvaluatedTickets> evaluatedTicketsArray) {
+        this.evaluatedTicketsArray = new ArrayList<EvaluatedTickets>();
+    }
+
+    public TicketHistory(){}
+
+    public ArrayList<EvaluatedTickets> getEvaluatedTicketsArray() {
+        return evaluatedTicketsArray;
+    }
+
+    public void setEvaluatedTicketsArray(ArrayList<EvaluatedTickets> evaluatedTicketsArray) {
+        this.evaluatedTicketsArray = evaluatedTicketsArray;
+    }
+
+    public Tickets ShowTickets(){
+
+        return null;
+    }
+
+
+}
